6f89368c805c444c49d34d245a86f052579a3975
likhil022 <likhilkosuru123@gmail.com>
2025-06-19 23:46:27 +0530

    Setup Completed. Hibernate and FPA

---
+++ src/main/java/com/ScootEase/ScootEase/ScootEaseApplication.java
@@ -0,0 +1,13 @@
+package com.ScootEase.ScootEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScootEaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScootEaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ScootEase/ScootEase/ScootEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ScootEase.ScootEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScootEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
